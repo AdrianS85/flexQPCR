ff6a8452d931bd18c7a24ec8f96792baa5a19964
AdrianS85 <adrianstankiewicz85@gmail.com>
2023-02-15 18:52:08 +0100

    Update main.R

--- main.R
+++ main.R
@@ -42,7 +42,7 @@ qpcr <- list(
       "geneCol" = "gene",
       "emptyName" = NA, ### this cannot be changed, as !is.na is used further, do use this value to change empty names to NA
       "blankName" = "H2O",
-      "ladder" = list("x2" = 1250, "x10" = 250, "x50" = 50, "x250" = 10, "x1250" = 2),
+      "ladder" = list("x2" = 1250, "x10" = 250, "x50" = 50, "x250" = 10, "x1250" = 2), ### !!! beware of non-ordered ladder names
       "refGenes" = c("gapdh", "hmbs", "tbp", "ywhaz"),
       "namesForReplicates" = list(1, 2, 3) ### !!! co jeśli nazwy replicatów zostały pdoane w plate file? Need to write some checking function
     )
